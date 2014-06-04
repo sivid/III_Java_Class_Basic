ffb6e3253339324231ff6bb9da47868085993a1b
sivid <sivid13@gmail.com>
2014-06-04 20:27:47 +0800

    homework 2 done

--- Basic/src/homework_2/Prime.java
+++ Basic/src/homework_2/Prime.java
@@ -11,14 +11,14 @@ public class Prime {
 	public static void main(String[] args) {
 		long[] primes = new long[1000];
 		int index = 0;
-		long inputValue = 55;
-		for (int i=1;i<inputValue;i+=2){
+		long inputValue = 12;					// didn't implement input 
+		for (int i=3;i<inputValue;i+=2){
 			if (isPrime(i)){
 				primes[index] = i;
 				index++;
 			}
 		}
-		for (int k=0;k<index;k++){
+		for (int k=0;k<index;k++){				// print out our answer
 			System.out.println(primes[k]);
 		}
 
